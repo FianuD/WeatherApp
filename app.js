75d2032273d2ad163bf343ef3f6cf27d21f2f651
FianuD <dovlofianu@gmail.com>
2019-06-06 00:00:27 +0000

    Feature: Added darksky api

--- app.js
+++ app.js
@@ -5,8 +5,14 @@ window.addEventListener('load', () => {
     // to get longitude and latitude using built-in functionality
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position => {
-            console.log(position);
-        })
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+
+            // weather api from darksky. Replaced long and latitude with values above
+            const api = `https://api.darksky.net/forecast/547570ce559a29780a721d4ec398d249/${lat},${long}`;
+        });
+
+        fetch()
     } else{
         // For when location can not be found
         h1.textContent = "Hey, this is not working because reasons!"
